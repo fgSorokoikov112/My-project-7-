e2f73f67c731f607db591632eae931cbac635089
fgSorokoikov112 <yrac228@mail.ru>
2024-07-15 15:47:08 +0300

    100

--- Assets/Scripts/ObjectCard.cs
+++ Assets/Scripts/ObjectCard.cs
@@ -7,10 +7,9 @@ using UnityEngine.EventSystems;
 public class ObjectCard : MonoBehaviour
 {
     public Building building;
-    int num_;
+    public int num_;
     public static List<ObjectCard> Cards =new List<ObjectCard>();
     void Start(){
-        num_ = Cards.Count;
         Cards.Add(this);
     }
     public static ObjectCard GetCard(int num){
---
+++ Assets/Scripts/Position.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Position : MonoBehaviour
+{
+    static public List<Position> Positions = new List<Position>();
+    public int Num;
+    void Start(){
+        if(Positions.Count == 0){
+            for(int i=0;i<8;i++){
+                
+            }
+        }
+    }
+}
